f71bda78a11036bd747dc11a5f00995340dd94d4
Ezekiel De Leon <ezekiel.d.e@ieee.org>
2018-10-04 06:39:27 -0700

    Removed a requirement from genres routes.

--- Vidly/middleware/async.js
+++ Vidly/middleware/async.js
@@ -1,8 +1,9 @@
-module.exports = function asyncMiddleware(handler) {
+// We dont need this since we are using an NPM package called
+// express-async-errors
+module.exports = function(handler) {
     return async (req, res , next) => {
         try {
-            await handler();
-            // ...
+            await handler(req, res);
         } catch (ex) {
             next(ex);
         }
